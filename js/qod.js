1dba58a48d93eb77180f177158973f9b9dea9e3a
Kristine Suarez <suarezkristine_001@live.ca>
2019-11-19 16:56:19 -0800

    Updated functions, header and footer

--- js/qod.js
+++ js/qod.js
@@ -1,26 +1,30 @@
 (function($){
 
-    $('body').append('hello world');
+    $('body').append('');
     //your code goes here
+
 
     //1: get request to grab random post and append to the DOM
 
     //add a click event for the "Show Me Another" btn and then run the AJAX code below
     $.ajax({
         method: "GET",
-        url: //qod_vars.rest.url = /wp/v2/posts
+        url: qod_vars.rest_url + "/wp/v2/posts"
 
     }).done(function(data){
         console.log(data);
         //append the quote to the DOM
     }).fail(function(error){
         console.log("an error occured", error);
-    })
+    });
     //2: post a new quote using the post method
-
-    //using a for m to submit a quote so a .submit event
     
-}) (jQuery)
+    //using a for m to submit a quote so a .submit event
+})(jQuery);
+
 
 //IIFE Immediately Invoked Function Expression
 // Invoked also means calling a function or just running a function
+
+
+// use for 	wp_localize_script( 'qod-script', 'qod_vars' on functions.php ->console.log( api_vars.success );
